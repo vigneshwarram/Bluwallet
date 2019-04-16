14b5b5a78b40faf2cc121be353e3d96791391b89
SivaBala <vickyrams20@gmail.com>
2019-04-16 18:11:46 +0530

    Gitignore added

--- index.js
+++ index.js
@@ -6,6 +6,11 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import {YellowBox} from 'react-native';
-YellowBox.ignoreWarnings(['Warning: ...']);
+import firebase from 'firebase'
+
+global.Symbol = require('core-js/es6/symbol');
+require('core-js/fn/symbol/iterator');
+require('core-js/fn/map');
+require('core-js/fn/set');
+require('core-js/fn/array/find');
 AppRegistry.registerComponent(appName, () => App);
