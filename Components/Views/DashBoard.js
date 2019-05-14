eaaff26aef1dafca75a3f0ea6b4403b110ab93f8
SivaBala <vickyrams20@gmail.com>
2019-05-14 17:32:08 +0530

    Logo seperated

--- Components/Views/DashBoard.js
+++ Components/Views/DashBoard.js
@@ -3,6 +3,7 @@ import { Path } from 'react-native-svg'
 import { View, StyleSheet, Image,Picker,NativeModules,Text,ActivityIndicator,TouchableOpacity,LayoutAnimation,} from 'react-native';
 import { Alert } from 'react-native';
 const { UIManager } = NativeModules;
+import Logo from '../logo'
 import { AreaChart, Grid } from 'react-native-svg-charts'
 import * as shape from 'd3-shape'
 import LinearGradient from 'react-native-linear-gradient';
@@ -230,18 +231,7 @@ App5Touch=()=>{
   }
     return (  
       <View style={styles.Maincontainers}>    
-         <View style={{  position: 'absolute',
-        top: 0,
-        bottom: 0,
-        left: 0,
-        right: 0,
-        alignItems:'center',
-    justifyContent: 'flex-start', }}>
-          <Image
-                style={styles.img}
-                source={require('./assets/logo.png')}
-            />
-            </View>
+       <Logo/>
       <View style={{justifyContent:'space-between',flexDirection:'row'}}>
       <View style={{backgroundColor:'#f6407b',height:this.state.h,width:this.state.w,justifyContent:'center', alignItems:'flex-end',borderTopEndRadius:25,borderBottomEndRadius:25, marginTop:10}}>
 
---
+++ Components/logo.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Path } from 'react-native-svg'
+import { View, StyleSheet, Image,Picker,NativeModules,Text,ActivityIndicator,TouchableOpacity,LayoutAnimation,} from 'react-native';
+import { Alert } from 'react-native';
+const { UIManager } = NativeModules;
+import { AreaChart, Grid } from 'react-native-svg-charts'
+import * as shape from 'd3-shape'
+import LinearGradient from 'react-native-linear-gradient';
+import { FlatList } from 'react-native-gesture-handler';
+ class logo extends React.Component {
+  render() {
+    return (  
+   
+         <View style={styles.Container}>       
+     <Image
+                style={styles.img}
+                source={require('./Views/assets/logo.png')}
+            />            
+      </View>
+    
+    );
+      }
+}
+
+const styles = StyleSheet.create({
+ Container:{
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    alignItems:'center',
+    justifyContent: 'flex-start'
+ },
+   img: {
+    width: '30%',
+    height: '32%',
+    resizeMode: 'cover',
+  },
+  
+  });
+export default  logo
