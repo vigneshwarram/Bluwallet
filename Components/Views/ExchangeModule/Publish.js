e77ca244ff2660f0e978deeb93c1ed08859fe243
SivaBala <vickyrams20@gmail.com>
2019-07-16 20:08:00 +0530

    Exchange list added

--- Components/Views/ExchangeModule/Publish.js
+++ Components/Views/ExchangeModule/Publish.js
@@ -22,6 +22,7 @@ export default class  Publish  extends React.Component {
       cityItems:["US Doller,Indian,Eutherium"],
       Coin: 'Us Doller',
       Amount:'COP',
+      StatusMode:'Publications',
       animate:false,
       w: 50,
       h: 45,
@@ -116,9 +117,21 @@ space(){
       <View style={styles.Maincontainers}>     
         <LinearGradient colors= {['#354E91','#314682','#283563','#222B50','#21284A']} style={styles.Maincontainers}>
       <LinearGradient
-  colors={['#2D3CAD','#4781DF','#529DF3','#7ED5F6','#97F5F9']} style={{height:'30%',opacity:0.9}}>    
+  colors={['#2D3CAD','#4781DF','#529DF3','#7ED5F6','#97F5F9']} start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={{height:'35%',opacity:0.9}}>    
       <LinearGradient
   colors={['#2D3CAD','#4781DF','#529DF3','#7ED5F6','#97F5F9']} style={{height:'100%',marginRight:30,marginTop:30}}>
+  <View style={{justifyContent:'center',alignItems:'center'}}>
+  <LinearGradient colors= {['#97F5F9','#7ED5F6','#529DF3','#4781DF','#2D3CAD']} style={{  width: 70,position:'absolute',top:200,bottom:-20,
+    height: 70,
+    borderRadius: 70/2,
+    backgroundColor:this.state.app1color,justifyContent:'center',alignItems:"center"}} >
+   
+    <Image  style={{width: 40, height: 40,resizeMode:'contain'}}  source={require('../assets/publication.png')} ></Image>
+    
+            
+          </LinearGradient>
+  </View>
+       
  <View style={{justifyContent:'center',alignItems:'center'}}>
 
           <View style={{flexDirection:'row',marginTop:10}}>
@@ -158,7 +171,7 @@ space(){
        
           
           </View>
-          <View style={{width:'80%',borderRadius:25,borderWidth:1,borderColor:'#fff',marginTop:10,marginBottom:20, justifyContent:'space-between',flexDirection:'row'}}>
+          <View style={{width:'70%',borderRadius:25,borderWidth:1,borderColor:'#fff',marginTop:10,marginBottom:20, justifyContent:'space-between',flexDirection:'row'}}>
 <View style={{flexDirection:'row',marginLeft:20}}>
 <Image  style={{width: 20, height: 20,marginTop:10}}  source={require("../assets/Searchicon.png")} ></Image> 
 <TextInput
@@ -189,25 +202,30 @@ space(){
         
   
           </View>
+          <View style={{width:'50%',borderRadius:25,borderWidth:1,borderColor:'#fff', alignItems:'center',flexDirection:'row',justifyContent:'center',padding:5}}>
+<View style={{justifyContent:'space-between',flexDirection:'row',alignItems:'center'}}>
+        <Text style={{color:'#fff',fontWeight:'bold',opacity:1,fontSize:12,fontFamily:'Exo2-Regular'}}>{this.state.StatusMode}</Text>
+        <Image  style={{width: 10, height: 10,resizeMode:'contain',marginLeft:10,marginRight:10}}  source={require("../assets/darrow.png")} ></Image> 
+        <Picker style={{ position:'absolute', top: 0, width: 1000, height: 1000}}
+   selectedValue={this.state.StatusMode}
+  onValueChange={(itemValue, itemIndex) => this.selectedMode(itemValue,itemIndex)}>
+  
+  <Picker.Item label="Publication" value="Publication" />
+  <Picker.Item label="Request" value="Request" />
+  
+  </Picker>
+        </View>
+        
+  
+          </View>
           </View>   
-          <LinearGradient colors= {['#97F5F9','#7ED5F6','#529DF3','#4781DF','#2D3CAD']} style={{  width: 100,marginLeft:20,position:'absolute',top:'70%',left:120,right:100,
-    height: 100,
-    borderRadius: 100/2,
-    backgroundColor:this.state.app1color,justifyContent:'center',alignItems:"center"}} >
-   
-    <Image  style={{width: 60, height: 60,resizeMode:'contain'}}  source={require('../assets/publication.png')} ></Image>
-    
-            
-          </LinearGradient> 
+      
           </LinearGradient>    
          
 
 
           </LinearGradient>    
-    <LinearGradient  colors= {['transparent','transparent','transparent']} style={{marginTop:'20%',position:'absolute',height:'100%',
-        top:'30%',left: 0,
-        right: 0}}>
-         <View style={{flex:1,marginBottom:90}}>
+         <View style={{flex:1,marginTop:100}}>
          <FlatList 
       ItemSeparatorComponent={this.space}
       data={this.state.dataSource}
@@ -216,37 +234,28 @@ space(){
       onHideUnderlay={separators.unhighlight} onPress = { this.clickedItemText.bind(this, item)}>
       <View style={{marginLeft:30,marginRight:30, shadowOffset: { width: 10, height: 10 },
   borderBottomWidth: 0,
-  shadowColor: '#394d88',
-  shadowOffset: { width: 0, height: 12 },
-  shadowOpacity: 0.8,
-  shadowRadius: 2,
-  elevation: 24,
   borderRadius:25}}>
   <LinearGradient
     colors={['#4262B5', '#3A549B','#314279','#2C3765','#2A335E']} style={{ borderRadius:25}}>
-        <View style={{flexDirection:'row',padding:10,justifyContent:'space-between'}}>
-          <View style={{flexDirection:'column'}}>
-                  
-     
-     <View style={{flex:1, flexDirection:'row',justifyContent:'space-between'}}>
-     <View style={{flexDirection:'row'}}>
+        <View style={{flexDirection:'row',padding:20,justifyContent:'space-between'}}>
+        <View>
+        <View style={{flexDirection:'row'}}>
      <Text  style={{marginRight:10,marginTop:10,color:"#ABB3D0",fontFamily:'Exo2-Bold',fontSize:11,marginLeft:10}}>{(item.Status=='Completed')?'Dan23':'Dan23'}</Text>  
      <Text  style={{marginRight:10,marginTop:10,fontSize:10,color:'#5496FF',fontFamily:'Exo2-Regular'}}>100</Text>         
      </View>   
-     <View>
-     <Text  style={{marginRight:10,marginTop:10,marginLeft:10, color:"#ABB3D0",fontFamily:'Exo2-Regular'}}>{(item.Status=='Completed')?'90.000':'90.000'}</Text> 
+        </View>
+        <View>
+        <Text  style={{marginRight:10,marginTop:10,marginLeft:10, color:"#ABB3D0",fontFamily:'Exo2-Regular'}}>{(item.Status=='Completed')?'90.000':'90.000'}</Text> 
      <Text  style={{marginTop:10,marginLeft:10, color:'#5496FF',fontFamily:'Exo2-Regular'}}>{(item.Status=='Completed')?'ETH':'BTC'}</Text>  
-     </View>  
-     <View style={{width:'35%',justifyContent:'center',alignItems:'center'}}>
+        </View>
+        <View style={{marginTop:10}}>
+     
           <LinearGradient colors={['#7498F9','#9B89F8','#D476F7']} start={{x: 0, y: 0}} end={{x: 1, y: 0}} style={{padding:7,borderRadius:10,backgroundColor:'green',justifyContent:'center',alignItems:'center'}}>
 <TouchableOpacity onPress={()=>this.props.navigation.navigate('Payment')}>
 <Text style={{color:'#fff',fontFamily:'Exo2-Regular'}}>Exchange</Text></TouchableOpacity>
 </LinearGradient>
-          </View>
-     </View>
-        
-  
-          </View>
+       
+        </View>
           
         </View>
 </LinearGradient>
@@ -269,7 +278,7 @@ space(){
   
   
 
-</LinearGradient>
+
 </LinearGradient>
      </View>
   
@@ -283,6 +292,12 @@ space(){
       selectedCop=(item,itemIndex)=>{
         this.setState({
           Amount:item
+        })
+      }
+      selectedMode=(item,itemIndex)=>
+      {
+        this.setState({
+         StatusMode:item
         })
       }
 }
