db0e96948f290a14d5dfdba7ef21c229a734dd75
SivaBala <vickyrams20@gmail.com>
2019-07-19 20:02:03 +0530

    Wallet Activity API Integrated

---
+++ Components/Views/Api/WalletActivity.js
@@ -0,0 +1,31 @@
+import Url from './CommonApi'
+import{AsyncStorage} from 'react-native'
+export const VaultSystemApi=async(params,VaultResponse)=>
+{
+    fetch(Url+'getactivitydata', {  
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'authorization':'bearer '+await AsyncStorage.getItem('AccessToken')
+        },
+        body: JSON.stringify({
+             cryptoType:params,
+             userId:await AsyncStorage.getItem('UserId') 
+             
+
+        })
+      }) .then((res)=> {
+        return res.json();
+       })
+       .then((resJson)=>{
+        VaultResponse(resJson)
+       
+        return resJson;
+       })
+       .catch((error) => {
+        console.error(error);
+    });
+}
+
+
